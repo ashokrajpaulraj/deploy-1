2f597c33ee89554928151589baf7c1482e761c0e
ashokrajpaulraj <ashokrajkp@gmail.com>
2022-06-29 20:41:46 +0530

    first commit

---
+++ src/main/java/com/learn/deploy/For/deployment/into/tomca/ForDeploymentIntoTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.learn.deploy.For.deployment.into.tomca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForDeploymentIntoTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForDeploymentIntoTomcatApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/learn/deploy/For/deployment/into/tomca/MyController.java
@@ -0,0 +1,14 @@
+package com.learn.deploy.For.deployment.into.tomca;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Home page";
+	}
+	
+}
