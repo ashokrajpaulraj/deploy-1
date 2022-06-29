20bb4c2a6bcf8af7284b23abd30d66780f704555
ashokrajpaulraj <ashokrajkp@gmail.com>
2022-06-29 23:34:27 +0530

    request mapping /test added

--- src/main/java/com/learn/deploy/For/deployment/into/tomca/MyController.java
+++ src/main/java/com/learn/deploy/For/deployment/into/tomca/MyController.java
@@ -1,9 +1,11 @@
 package com.learn.deploy.For.deployment.into.tomca;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class MyController {
 
 	@GetMapping("/")
